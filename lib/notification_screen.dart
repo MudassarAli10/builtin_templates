f254e7fbce93a2e4c1f4fb5f569243eb86e11198
MudassarAli10 <mudassarali786786as@gmail.com>
2024-08-26 15:26:21 +0500

    update notification_screen.dart

--- lib/notification_screen.dart
+++ lib/notification_screen.dart
@@ -7,6 +7,12 @@ class EmptyNotificationsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+elevation: 0,
+        leading:
+        IconButton(onPressed: (){Navigator.pop(context);}, icon:const Icon(Icons.arrow_back_ios_new_outlined)),
+      ),
       backgroundColor: Colors.white,
       body: SafeArea(
         child: Padding(
